3f99686de3296bcc722bb940761ab1b614a9e92c
jiwoojun <78287870+jiwoojun@users.noreply.github.com>
2021-03-10 19:47:46 +1300

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -14,6 +14,12 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         tiles.setTileAt(tiles.getTileLocation(10, 5), assets.tile`myTile25`)
     }
 })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    selectedIndex = Math.max(selectedIndex - 1, 0)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    selectedIndex = Math.min(selectedIndex + 1, item.length - 1)
+})
 controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
     if (inventoryVisible) {
         closeInventory()
@@ -24,16 +30,48 @@ controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
 function openInventory () {
     inventoryVisible = true
     controller.moveSprite(Babypenguin, 0, 0)
+    selectedIndex = 0
 }
 spriteutils.createRenderable(100, function (screen2) {
     if (inventoryVisible) {
-        screen2.fillRect(10, 10, 140, 100, 1)
+        screen2.fillRect(10, 10, 140, 100, 11)
         screen2.drawRect(10, 10, 140, 100, 15)
         images.print(screen2, "INVENTORY", 14, 14, 15)
+        images.print(screen2, item_name[selectedIndex], 70, 14, 1)
         screen2.fillRect(14, 24, 132, 1, 15)
+        item_top = 28
+        for (let index = 0; index <= item.length - 1; index++) {
+            spriteutils.drawTransparentImage(item[index], screen2, 14 + index * 20, item_top)
+        }
+        spriteutils.drawTransparentImage(img`
+            ff.ff.ff.ff.ff.ff.ff
+            f..................f
+            ....................
+            f..................f
+            f..................f
+            ....................
+            f..................f
+            f..................f
+            ....................
+            f..................f
+            f..................f
+            ....................
+            f..................f
+            f..................f
+            ....................
+            f..................f
+            f..................f
+            ....................
+            f..................f
+            ff.ff.ff.ff.ff.ff.ff
+            `, screen2, 14 + selectedIndex * 20 - 2, item_top - 2)
     }
 })
+let item_top = 0
+let selectedIndex = 0
 let inventoryVisible = false
+let item_name: string[] = []
+let item: Image[] = []
 let Babypenguin: Sprite = null
 Babypenguin = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -98,7 +136,7 @@ Babypenguin,
 500,
 true
 )
-let item = [
+item = [
 assets.tile`myTile29`,
 assets.tile`myTile28`,
 img`
@@ -162,7 +200,7 @@ img`
     . . . . . . . . . . . . 5 . . 1 
     . . . . . . . . . . . 5 5 . 1 1 
     . 2 2 2 2 2 2 2 2 2 2 5 5 1 1 1 
-    . 2 2 2 2 2 2 2 2 2 2 5 5 1 1 1 
+    d 2 2 2 2 2 2 2 2 2 2 5 5 1 1 1 
     . 2 2 2 2 2 2 2 2 2 2 5 5 1 1 1 
     . . . . . . . . . . . 5 5 . 1 1 
     . . . . . . . . . . . . 5 . . 1 
@@ -173,4 +211,12 @@ img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `
+]
+item_name = [
+"key",
+"hint",
+"diamond",
+"coin",
+"bow and arrow",
+"torch"
 ]
